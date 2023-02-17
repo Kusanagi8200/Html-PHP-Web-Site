be8639a5d143fb2df442c653dee6613aab152312
Kusanagi8200 <elijahkaminski230@gmail.com>
2023-02-17 19:16:16 +0100

    Itg-Balise-Google

--- registration/register.php
+++ registration/register.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z9NK68ZC4J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z9NK68ZC4J');
+</script>
+
 <link rel="stylesheet" href="style.css" />
 </head>
 <style>
